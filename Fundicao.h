0b1c9b6bd78c3f3c4f3d903380fdfd71bdcf65e0
Filipe Fernandes <94529914+Fl13ip@users.noreply.github.com>
2022-01-10 16:43:47 +0000

    Update Fundicao.h

--- Fundicao.h
+++ Fundicao.h
@@ -11,12 +11,30 @@
 
 class Fundicao : public Edificio{
 
+    bool deal = false;
+    int acoProd=0;
 public:
     Fundicao(int day) : Edificio("fun",0,0,day){};
 
     void newDay() override;
 
     void produzir() override;
+
+    bool getDealState() const{
+        return deal;
+    }
+
+    int getAcoProd() const{
+        return acoProd;
+    }
+
+    void addAcoProd(const int add) {
+        acoProd = acoProd + add;
+    }
+
+    void removeAcoProd(const int cost) {
+        acoProd = acoProd - cost;
+    }
 
 
 
