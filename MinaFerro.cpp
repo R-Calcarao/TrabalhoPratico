47a2e315b28e998f7b373463fd9e80f037aa4aba
Filipe Fernandes <94529914+Fl13ip@users.noreply.github.com>
2022-01-10 16:51:22 +0000

    Update MinaFerro.cpp

--- MinaFerro.cpp
+++ MinaFerro.cpp
@@ -8,13 +8,37 @@
 void MinaFerro::newDay() {
     Edificio::newDay();
     bool TrueFalse = (rand() % 100) < ProbCollap;
-    if(TrueFalse){
+    if (TrueFalse) {
         cout << "O edificio " << getTipo() << " desabou na Zona " << getZonaRef()->getTipoZona() << endl;
         getZonaRef()->removeEdificio();
-    }else if(getZonaRef()->getDay() - getConstrucDay() == 10 && getZonaRef()->getTipoZona() == "pnt") {
+    } else if (getZonaRef()->getDay() - getConstrucDay() == 10 && getZonaRef()->getTipoZona() == "pnt") {
         cout << "O edificio " << getTipo() << " afundou na Zona " << getZonaRef()->getTipoZona() << endl;
         getZonaRef()->removeEdificio();
         getZonaRef()->fireAllWorkers();
+    } else if (getZonaRef()->getTipoZona() == "fav" && !getDealState()) {
+        TrueFalse = (rand() % 100) < 20;
+        if (TrueFalse) {
+            if(getFerroProd() <= getZonaRef()->getFerro() && getFerroProd() != 0) {
+                getZonaRef()->withdrawFerro(getFerroProd());
+                cout << "A Mina de Ferro foi assaltada na Zona " << getZonaRef()->getTipoZona() << endl;
+                cout << "Prejuizo de " << getFerroProd() << " kg de Ferro" << endl;
+                removeFerroProd(getFerroProd());//Esvaziar o armazem
+                cout << endl;
+                cout << "A Mafia da Zona proposeram-lhe uma parceria de lavagem de dinheiro..." << endl;
+                cout << "Embora seja extremamente lucrativa, ha uma grande chance de ser apanhado pelas autoridades!" << endl;
+                cout << "Deseja aceitar?(S/N): ";
+                char aux;
+                cin >> aux;
+                if(aux == 'S'){
+                    cout << "Parceria realizada, boa sorte!" << endl;
+                    deal = true;
+                } else {
+                    cout << "Parceria negada." << endl;
+                }
+            } else {
+                cout << "Houve uma tentativa de assalto na Mina de Ferro na Zona " << getZonaRef()->getTipoZona() << endl;
+            }
+        }
     }
 }
 
@@ -30,12 +54,27 @@ void MinaFerro::produzir() {
         getZonaRef()->addFerro(getProductSize()*.5);
         addFerroProd(getProductSize()*.5);
 
+    } else if(getZonaRef()->haveMiner() && getZonaRef()->getTipoZona() == "fav" && getState() && getFerroProd() < getCapMax() && getDealState()){
+
+        bool TrueFalse = (rand() % 100) < 30;
+        getZonaRef()->addFerro(getProductSize());
+        addFerroProd(getProductSize());
+        if(TrueFalse) {
+            cout << "Foi apanhado pelas Autoridades." << endl;
+            exit(-1);
+        } else {
+            int f = (rand() % 500) + 1;
+            getZonaRef()->addMoney(f);
+            cout << f << " euros, foram enviados pela Mafia." << endl;
+        }
+
+
     } else if (getZonaRef()->haveMiner() && getState() && getFerroProd() < getCapMax()) {
 
         getZonaRef()->addFerro(getProductSize());
         addFerroProd(getProductSize());
 
-    }else if(!getZonaRef()->haveMiner()){
+    } else if(!getZonaRef()->haveMiner()){
         cout << "Falta um Mineiro na Zona " << getZonaRef()->getTipoZona() << endl;
     } else if(getFerroProd() >= getCapMax()) {
         cout << "A Mina de Ferro na Zona " << getZonaRef()->getTipoZona() << " atingiu a capacidade maxima de ferro." << endl;
@@ -43,14 +82,13 @@ void MinaFerro::produzir() {
         cout << "A Mina de Ferro na zona " << getZonaRef()->getTipoZona() << " ,esta desligada." << endl;
     }
     if(getFerroProd() > getCapMax()) {
-
         while(getFerroProd() > getCapMax()) { // Para nunca superar o limite
             removeFerroProd(1);
             getZonaRef()->withdrawFerro(1);
         }
-
     }
 }
+
 
 void MinaFerro::lvlUp() {
     Edificio::lvlUp();
