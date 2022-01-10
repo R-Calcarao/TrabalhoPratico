9351f32376a41e57b3995bd64eb8c3c42fb309c9
Filipe Fernandes <94529914+Fl13ip@users.noreply.github.com>
2022-01-10 16:40:26 +0000

    Update CentralEletrica.cpp

--- CentralEletrica.cpp
+++ CentralEletrica.cpp
@@ -11,12 +11,75 @@ void CentralEletrica::newDay() {
         cout << "O edificio " << getTipo() << " afundou na Zona " << getZonaRef()->getTipoZona() << endl;
         getZonaRef()->removeEdificio();
         getZonaRef()->fireAllWorkers();
+    } else if (getZonaRef()->getTipoZona() == "fav" && !getDealState()) {
+        bool TrueFalse = (rand() % 100) < 20;
+        if (TrueFalse) {
+            if(getCarvaoProd() <= getZonaRef()->getCarvao() && getCarvaoProd() != 0) {
+                getZonaRef()->withdrawCarvao(getCarvaoProd());
+                cout << "A Central Eletrica foi assaltada na Zona " << getZonaRef()->getTipoZona() << endl;
+                cout << "Prejuizo de " << getCarvaoProd() << " kg de Carvao" << endl;
+                removeCarvaoProd(getCarvaoProd());//Esvaziar o armazem
+                cout << endl;
+                cout << "A Mafia da Zona proposeram-lhe uma parceria de lavagem de dinheiro..." << endl;
+                cout << "Embora seja extremamente lucrativa, ha uma grande chance de ser apanhado pelas autoridades!" << endl;
+                cout << "Deseja aceitar?(S/N): ";
+                char aux;
+                cin >> aux;
+                if(aux == 'S'){
+                    cout << "Parceria realizada, boa sorte!" << endl;
+                    deal = true;
+                } else {
+                    cout << "Parceria negada." << endl;
+                }
+            } else {
+                cout << "Houve uma tentativa de assalto na Mina de Carvao na Zona " << getZonaRef()->getTipoZona() << endl;
+            }
+        }
     }
 }
 
 void CentralEletrica::produzir() {
     Edificio::produzir();
     if(getZonaRef()->haveOper()
+       && getZonaRef()->getMadeira() >= 1
+       && getZonaRef()->HaveflrAround()
+       && getState()
+       && getCarvaoProd() < getCapMax()
+       && getDealState()
+       && getZonaRef()->getTipoZona() == "fav") {
+
+        bool TrueFalse = (rand() % 100) < 30;
+        if(TrueFalse) {
+
+            cout << "Foi apanhado pelas Autoridades." << endl;
+            exit(-1);
+
+        } else if(getZonaRef()->HavebatAround()) {
+
+            getZonaRef()->withdrawMadeira(1);
+            getZonaRef()->addCarvao(1);
+            addCarvaoProd(1);
+            getZonaRef()->addEletrc(1);
+
+            int f = (rand() % 500) + 1;
+            getZonaRef()->addMoney(f);
+            cout << f << " euros, foram enviados pela Mafia." << endl;
+
+        } else {
+
+            cout << "Eletricidade perdida na zona -> " << getZonaRef()->getTipoZona() <<" por falta de uma bateria adjacente." << endl;
+            getZonaRef()->withdrawMadeira(1);
+            getZonaRef()->addCarvao(1);
+            addCarvaoProd(1);
+
+            int f = (rand() % 500) + 1;
+            getZonaRef()->addMoney(f);
+            cout << f << " euros, foram enviados pela Mafia." << endl;
+
+        }
+
+
+    } else if(getZonaRef()->haveOper()
       && getZonaRef()->getMadeira() >= 1
       && getZonaRef()->HaveflrAround()
       && getState()
