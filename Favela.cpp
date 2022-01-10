15b6bc27c709e7ca432af6c2f7692d82dc742ac7
Filipe Fernandes <94529914+Fl13ip@users.noreply.github.com>
2022-01-10 17:00:10 +0000

    Add files via upload

---
+++ Favela.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ACER on 08/01/2022.
+//
+
+#include "Favela.h"
---
+++ Favela.h
@@ -0,0 +1,19 @@
+//
+// Created by ACER on 08/01/2022.
+//
+
+#ifndef PRATICOTRABALHO_FAVELA_H
+#define PRATICOTRABALHO_FAVELA_H
+#include "Zona.h"
+
+
+
+class Favela : public Zona{
+
+public:
+    Favela(int linha,int coluna) : Zona("fav",linha,coluna){};
+
+};
+
+
+#endif //PRATICOTRABALHO_FAVELA_H
